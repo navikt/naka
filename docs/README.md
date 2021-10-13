c45f5c34aff2bc509a90456e03f4506a62ee9952
Kyrre Havik <kyrremann@gmail.com>
2021-10-13 09:02:11 +0200

    feat: init commit

---
+++ docs/README.md
@@ -0,0 +1,22 @@
+---
+title: NAV Kataloger
+slug: /
+---
+
+## Behandlingskatalog
+* [Om behandlingskatalogen](behandlingskatalog.md)
+
+* [Applikasjon](https://behandlingskatalog.nais.adeo.no). Behandlingskatalogen
+* [Applikasjon Test](https://behandlingskatalog.dev.adeo.no/). Behandlingskatalogen (Test)
+
+## Etterlevelse
+* [Om etterlevelse](etterlevelse.md)
+
+* [Applikasjon](https://etterlevelse.intern.nav.no/). Etterlevelse
+* [Applikasjon Test](https://etterlevelse.dev.intern.nav.no/). Etterlevelse (Test)
+
+## Teamkatalog
+* [Om teamkatalogen](teamkatalog.md)
+
+* [Applikasjon](https://teamkatalog.nais.adeo.no). Teamkatalogen
+* [Applikasjon Test](https://teamkatalog.dev.adeo.no/). Teamkatalogen (Test)
---
+++ docusaurus.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NAKA',
+  tagline: 'NAV Kataloger',
+  url: 'https://navikt.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'navikt',
+  projectName: 'naka',
+  trailingSlash: false,
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/navikt/nakan/edit/main/',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'NAKA',
+        logo: {
+          alt: 'NAKA logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'README',
+            position: 'left',
+            label: 'docs',
+          },
+          {
+            href: 'https://github.com/navikt/naka',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {},
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
